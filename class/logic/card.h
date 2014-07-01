eb2e8a0aa90d64ae4db7c58739c719f35d1bcfcf
plusun <tomsun.0.7@gmail.com>
2014-07-01 14:41:35 +0800

    more cards

--- class/logic/card.h
+++ class/logic/card.h
@@ -1,10 +1,15 @@
 #include "basic.h"
+#include "character.h"
 
 class Card
 {
 private:
   Value _cost;
 public:
+  Card(Value c):
+    _cost(c)
+  {
+  }
   Value cost()
   {
     return _cost;
@@ -13,4 +18,28 @@ public:
   {
     _cost = new_cost;
   }
-}
+};
+
+class MinionCard: public Card
+{
+private:
+  Minion *_minion;
+public:
+  MinionCard(Value cost, Minion *m):
+    Card(cost), _minion(m)
+  {
+  }
+  ~MinionCard()
+  {
+    if (_minion != NULL)
+      delete _minion;
+  }
+  Minion *minion()
+  {
+    return _minion;
+  }
+  void minion(Minion *new_minion)
+  {
+    _minion = new_minion;
+  }
+};
