b77da9ae6c439eac99e72dcff20d9cfe21dccb48
plusun <tomsun.0.7@gmail.com>
2014-07-03 16:54:07 +0800

    change the logic of battlefield

--- class/logic/battlefield.h
+++ class/logic/battlefield.h
@@ -4,31 +4,39 @@
 class Battlefield
 {
 public:
-	int amount;
-	vector<Minion*> _minion;
-	Battlefield():amount(0)
-	{
-		_minion.clear();
-	}
-	void summon_minion(minion* m, int position = -1,)
-	{
-		amount++;
-		if (position == -1)
-			_minion.push_back(m);
-		else
-		{
-			_minion.push_back(m);
-			for (int i = amount - 1; i != position; i--)
-				_minion[i] = _minion[i-1];
-			_minion[position] = m;
-		}
-	}
-	void destroy_minion(int position)
-	{
-		delete _minion[position];
-		for (int i = position; i < amount - 1; i++)
-			_minion[i] = _minion[i+1];
-		_minion.pop_back();
-		amount--;
-	}
+  vector<Minion *> _minion;
+  vector<Hero *> _hero;
+  Battlefield()
+  {
+    _minion.clear();
+    _hero.clear();
+  }
+  ~Battlefield()
+  {
+    for (vector<Mionion *>::iterator itr = _minion.begin();
+	 itr != _minion.end();
+	 ++itr)
+      if (*itr != NULL)
+	delete *itr;
+    for (vector<Hero *>::iterator itr = _hero.begin();
+	 itr != _hero.end();
+	 ++itr)
+      if (*itr != NULL)
+	delete *itr;
+  }
+  void summon_minion(minion* m, int position = -1,)
+  {
+    if (position < 0 || position > _minion.size())
+      _minion.push_back(m);
+    else
+      _minion.insert(_minion.begin() + position);
+  }
+  void destroy_minion(int position)
+  {
+    if (position < 0 || position >= _minion.size())
+      return;
+    if (_minion[position] != NULL)
+      delete _minion[position];
+    _minion.erase(_minion.begin() + position);
+  }
 }
