5a57dd7eeaaa921f766c115eeb9eceec34cece55
plusun <tomsun.0.7@gmail.com>
2014-07-04 11:27:43 +0800

    battlefield

--- class/logic/battlefield.h
+++ class/logic/battlefield.h
@@ -1,45 +1,44 @@
 #include "basic.h"
 #include "character.h"
 
+#define SIDE 2
+
 class Battlefield
 {
 public:
-  vector<Minion *> _minion;
-  vector<Hero *> _hero;
+  vector<Minion *> _minion[SIDE];
+  Hero *_hero[SIDE];
   Battlefield(Hero *one, Hero *another)
   {
-    _minion.clear();
-    _hero.clear();
-    _hero.push_back(one);
-    _hero.push_back(another);
+    _hero[0] = one;
+    _hero[1] = another;
   }
   ~Battlefield()
   {
-    for (vector<Minion *>::iterator itr = _minion.begin();
-	 itr != _minion.end();
-	 ++itr)
-      if (*itr != NULL)
-	delete *itr;
-    for (vector<Hero *>::iterator itr = _hero.begin();
-	 itr != _hero.end();
-	 ++itr)
-      if (*itr != NULL)
-	delete *itr;
+    for (int i = 0; i < SIDE; ++i)
+      for (vector<Mionion *>::iterator itr = _minion[i].begin();
+	   itr != _minion[i].end();
+	   ++itr)
+	if (*itr != NULL)
+	  delete *itr;
+    for (int i = 0; i < SIDE; ++i)
+      if (_hero[i] != NULL)
+	delete _hero[i];
   }
-  void summon_minion(Minion* m, int position = -1,)
+  void summon_minion(minion* m, int side, int position = -1)
   {
-    if (position < 0 || position > _minion.size())
-      _minion.push_back(m);
+    if (position < 0 || position > _minion[side].size())
+      _minion[side].push_back(m);
     else
-      _minion.insert(_minion.begin() + position);
+      _minion[side].insert(_minion[side].begin() + position);
   }
   void destroy_minion(int position)
   {
-    if (position < 0 || position >= _minion.size())
+    if (position < 0 || position >= _minion[side].size())
       return;
-    if (_minion[position] != NULL)
-      delete _minion[position];
-    _minion.erase(_minion.begin() + position);
+    if (_minion[side][position] != NULL)
+      delete _minion[side][position];
+    _minion[side].erase(_minion[side].begin() + position);
   }
   static bool attack(Character *a, Character *b)
   {
