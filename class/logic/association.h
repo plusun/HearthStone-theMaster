0aa890622bfb9387757ed50dbad96365ae19239e
赵晓辉 <philip.20050132@163.com>
2014-07-07 09:41:16 +0800

    changes

--- class/logic/association.h
+++ class/logic/association.h
@@ -3,6 +3,19 @@
 
 using namespace std;
 
-map<int, Card> mapcard;
-map<int, Minion> mapminion;
-
+Card* ntoc(int n)
+{
+	Card* tmp;
+	switch (n)
+	{
+	case 1: tmp = new Wisp_card; break;
+	case 2:	tmp = new Murloc_raider_card; break;
+	case 3: tmp = new Bloodfen_raptor_card; break;
+	case 4: tmp = new River_crocolisk_card; break;
+	case 5: tmp = new Magma_Rager_card; break;
+	case 6: tmp = new Chiliwind_yeti_card; break;
+	case 7: tmp = new Oasis_snapjaw_card; break;
+	case 8: tmp = new Boulderfist_ogre_card; break;
+	case 9: tmp = new Core_hound_card; break;
+	case 10: tmp = new War_golem_card; break;
+}
--- class/logic/card_test.h
+++ class/logic/card_test.h
@@ -13,6 +13,7 @@ Wisp_card::cast()
 
 }
 
+
 class Murloc_raider_card : public MinionCard
 {
 public:
@@ -24,4 +25,98 @@ Murloc_raider_card::cast()
 	_minion = new Murloc_raid;
 }
 
-class Bloodfen_raptor_card 
+
+class Bloodfen_raptor_card : public MinionCard
+{
+public:
+	Bloodfen_raptor(2,NULL);
+}
+
+Bloodfen_raptor_card::cast()
+{
+	_minion = new Bloodfen_raptor;
+}
+
+
+class River_crocolisk_card : public MinionCard
+{
+public:
+	River_crocolisk(2,NULL);
+}
+
+River_crocolisk_card::cast()
+{
+	_minion = new River_crocolisk;
+}
+
+
+class Magma_Rager_card : public MinionCard
+{
+public:
+	Magma_Rager(3,NULL);
+}
+
+Magma_Rager_card::cast()
+{
+	_minion = new Magma_Rager;
+}
+
+
+class Chiliwind_yeti_card : public MinionCard
+{
+public:
+	Chiliwind_yeti(4,NULL);
+}
+
+Chiliwind_yeti_card::cast()
+{
+	_minion = new Chiliwind_yeti;
+}
+
+
+class Oasis_snapjaw_card : public MinionCard
+{
+public:
+	Oasis_snapjaw(4,NULL);
+}
+
+Oasis_snapjaw_card::cast()
+{
+	_minion = new Oasis_snapjaw;
+}
+
+
+class Boulderfist_ogre_card : public MinionCard
+{
+public:
+	Boulderfist_ogre(6,NULL);
+}
+
+Boulderfist_ogre_card::cast()
+{
+	_minion = new Boulderfist_ogre;
+}
+
+
+class Core_hound_card : public MinionCard
+{
+public:
+	Core_hound(7,NULL);
+}
+
+Core_hound_card::cast()
+{
+	_minion = new Core_hound;
+}
+
+
+class War_golem_card : public MinionCard
+{
+public:
+	War_golem(7,NULL);
+}
+
+War_golem_card::cast()
+{
+	_minion = new War_golem;
+}
