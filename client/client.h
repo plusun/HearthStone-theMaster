516dc0a1eb08fe39b89a9f38676b56d3af386286
赵晓辉 <philip.20050132@163.com>
2014-07-10 14:36:15 +0800

    class client

---
+++ client/client.h
@@ -0,0 +1,27 @@
+#include <cstdlib>
+#include <vector>
+#include <Winsock2.h>
+#include <stdio.h>
+#include <iostream> 
+#include "string.h"
+using namespace std; 
+#pragma comment(lib, "ws2_32.lib")
+
+
+class client
+{
+public:
+	SOCKET sockClient;
+	char recvBuf[100];
+	vector<int> torecv(char* s, char c);
+	char* tosend(vector<int> vi, char c);
+	client(string str);
+	client();
+	int start_game();						//return = 1, go first;return = 0, go second
+	void draw_card(int a);
+	void use_card(int a, int b, int c, int d);
+	void attack(int a, int b);
+	void end_turn();
+	void win(int n);
+	vector<int> opponent_turn();
+};
