fb85452ba6d02f72f96942005113f3d9022d64dc
plusun <tomsun.0.7@gmail.com>
2014-07-15 14:34:22 +0800

    subtle changes

--- class/logic/card.cpp
+++ class/logic/card.cpp
@@ -5,8 +5,9 @@ std::map<int, Card *> cardPool;
 
 Card *NumberToCard(int no)
 {
-	if (cardPool.find(no) == cardPool.end())
-		cardPool[no] = new MinionCard(1, new Wisp());
+  if (cardPool.find(no) == cardPool.end())
+    cardPool[no] = new MinionCard(1, NULL);
+  cardPool[no]->minion(new Wisp());
   return cardPool[no];
 }
 
